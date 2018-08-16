53d271b2264d31231c744f7cb8babedbc1a9dcdb
Dennis Hermsmeier <dennis.hermsmeier@gmail.com>
2018-08-16 22:02:27 +0200

    install correct varnish version

--- build-images.sh
+++ build-images.sh
@@ -42,7 +42,7 @@ for VARNISH_VERSION_DIR in varnish-*; do
     docker build \
         --no-cache \
         --pull \
-        --build-arg VARNISH_VERSION="${PATCH_RELEASE_TAG}" \
+        --build-arg VARNISH_VERSION="${META_RELEASE_TAG}" \
         --build-arg BASE_IMAGE_VERSION="${BASE_IMAGE_VERSION}" \
         --tag "${IMAGE_NAME}:${MAJOR_RELEASE_TAG}" \
         --tag "${IMAGE_NAME}:${MINOR_RELEASE_TAG}" \
