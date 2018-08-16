1252f9795449735b8b51dbdafcef4a71aa69dd19
Dennis Hermsmeier <dennis.hermsmeier@gmail.com>
2018-08-16 21:34:22 +0200

    Feature/build with travis (#1)
    * Add travis support
    
    * some less output
    
    * try to use latest ubuntu
    
    * install docker
    
    * remove new image
    
    * Apt update and upgrade is not needed
    
    * get shit done
    
    * finally get shit done ffs

---
+++ build-images.sh
@@ -0,0 +1,53 @@
+#!/usr/bin/env bash
+
+set -xe
+
+exact_version() {
+
+    unset APP
+    APP="${1}"
+    FILE="${2}"
+    
+    grep -i "${APP}" "${FILE}" | cut -d '=' -f 2
+
+}
+
+for VARNISH_VERSION_DIR in varnish-*; do
+
+    echo "# # # # # # # # # # # # # # # #"
+    echo "Building ${VARNISH_VERSION_DIR}"
+    echo "# # # # # # # # # # # # # # # #"
+
+    unset FULL_VARNISH_VERSION_PATH
+    FULL_VARNISH_VERSION_PATH="${TRAVIS_BUILD_DIR}/${VARNISH_VERSION_DIR}"
+
+    unset VERSION_FILE
+    VERSION_FILE="${FULL_VARNISH_VERSION_PATH}/exact_versions"
+
+    unset EXACT_VARNISH_VERSION
+    EXACT_VARNISH_VERSION="$(exact_version VARNISH ${VERSION_FILE})"
+
+    unset MAJOR_RELEASE_TAG
+    MAJOR_RELEASE_TAG="${EXACT_VARNISH_VERSION%%.*}"
+
+    unset BASE_IMAGE_VERSION
+    BASE_IMAGE_VERSION="$(exact_version BASE_IMAGE ${VERSION_FILE})"
+
+    docker build \
+        --no-cache \
+        --pull \
+        --build-arg VARNISH_VERSION="${EXACT_VARNISH_VERSION}" \
+        --build-arg BASE_IMAGE_VERSION="${BASE_IMAGE_VERSION}" \
+        --tag "${IMAGE_NAME}:${MAJOR_RELEASE_TAG}" \
+        --tag "${IMAGE_NAME}:${EXACT_VARNISH_VERSION}" \
+        --file "${FULL_VARNISH_VERSION_PATH}/Dockerfile" \
+        "${TRAVIS_BUILD_DIR}"
+
+    if [[ "${TRAVIS_BRANCH}" == "master" ]] && [[ "${TRAVIS_PULL_REQUEST}" == "false" ]]; then
+
+        docker push "${IMAGE_NAME}:${MAJOR_RELEASE_TAG}"
+        docker push "${IMAGE_NAME}:${EXACT_VARNISH_VERSION}"
+
+    fi
+
+done
